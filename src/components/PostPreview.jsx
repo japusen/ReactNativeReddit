eb16049e70f9f63626e10ef6b4ed618ad822f1c0
Jay <japusen32@gmail.com>
2024-02-27 01:50:27 -0800

    Started displaying different types of media

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -1,7 +1,10 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image, Pressable } from "react-native";
 import { Text, Card } from "react-native-paper";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { WebView } from "react-native-webview";
+import { ResizeMode } from "expo-av";
+import VideoPlayer from "expo-video-player";
 
 const styles = StyleSheet.create({
 	container: {
@@ -13,30 +16,138 @@ const styles = StyleSheet.create({
 	separator: {
 		marginTop: 10,
 	},
+	image: {
+		height: 100,
+		width: 100,
+	},
+	fullImage: {
+		height: 400,
+		backgroundColor: "black",
+	},
 });
 
 export const PostPreview = ({ item }) => {
 	const navigation = useNavigation();
+	const [showMedia, setShowMedia] = useState(false);
 
+	const toggleShowMedia = () => {
+		setShowMedia(!showMedia);
+	};
+
+	console.log(item.post_hint);
 	return (
-		<Card
-			onPress={() =>
-				navigation.navigate("Post", {
-					postID: item.id,
-				})
-			}
-			style={{ marginHorizontal: 10 }}
-		>
-			<Text>{item.title}</Text>
-			<Text>{item.subreddit}</Text>
-			<Text>{item.author}</Text>
-			<Text>{item.score}</Text>
-			<Text>{item.thumbnail}</Text>
-			{item.over_18 && <Text>nsfw</Text>}
-			{item.spoiler && <Text>spoiler</Text>}
-			{item.locked && <Text>locked</Text>}
-		</Card>
+		<View style={{ marginHorizontal: 10 }}>
+			<Card
+				onPress={() => {
+					// navigation.navigate("Post", {
+					// 	postID: item.id,
+					// });
+					//console.log(item.media);
+				}}
+			>
+				<Text>{item.title}</Text>
+				<Text>{item.subreddit}</Text>
+				<Text>{item.author}</Text>
+				<Text>{item.score}</Text>
+				{item.over_18 && <Text>nsfw</Text>}
+				{item.spoiler && <Text>spoiler</Text>}
+				{item.locked && <Text>locked</Text>}
+
+				{!item.is_self && (
+					<Pressable onPress={toggleShowMedia}>
+						<Thumbnail
+							preview={item.preview}
+							thumbnail={item.thumbnail}
+						/>
+					</Pressable>
+				)}
+			</Card>
+			{item.media && showMedia && <Media media={item.media} />}
+			{item.post_hint === "link" &&
+				item.domain === "i.imgur.com" &&
+				showMedia && (
+					<VideoPlayer
+						videoProps={{
+							shouldPlay: true,
+							resizeMode: ResizeMode.CONTAIN,
+							source: {
+								uri: item.url.replace(".gifv", ".mp4"),
+							},
+						}}
+						// style={{ flex: 1 }}
+					/>
+				)}
+			{!item.media && item.post_hint !== "link" && showMedia && (
+				<Image
+					resizeMode="contain"
+					style={styles.fullImage}
+					source={{
+						uri: item.url,
+					}}
+				/>
+			)}
+		</View>
 	);
+};
+
+const Thumbnail = ({ preview, thumbnail }) => {
+	const uri = preview ? preview.images.at(0).source.url : thumbnail;
+	return (
+		<Image
+			style={styles.image}
+			source={{
+				uri,
+			}}
+		/>
+	);
+};
+
+const Media = ({ media }) => {
+	if (media.reddit_video) {
+		// console.log("reddit_video", Object.keys(media.reddit_video));
+		return (
+			<VideoPlayer
+				videoProps={{
+					shouldPlay: true,
+					resizeMode: ResizeMode.CONTAIN,
+					source: {
+						uri: media.reddit_video.dash_url,
+					},
+				}}
+			/>
+		);
+	} else {
+		if (media.oembed) {
+			console.log("oembed");
+			// console.log(media.oembed.html);
+			const url = extractSrcFromHTML(media.oembed.html);
+			console.log(url);
+			return (
+				<WebView
+					// originWhitelist={["*"]}
+					allowsFullscreenVideo
+					allowsInlineMediaPlayback
+					mediaPlaybackRequiresUserAction
+					// source={{ html: media.oembed.html }}
+					source={{ uri: url }}
+					style={{ height: 500, backgroundColor: "black" }}
+				/>
+			);
+		} else {
+			console.log("media", Object.keys(media));
+		}
+	}
+};
+
+const extractSrcFromHTML = (htmlString) => {
+	const regex = /src\s*=\s*["']([^"']+)["']/;
+	const match = regex.exec(htmlString);
+
+	if (match && match.length > 1) {
+		return match[1];
+	} else {
+		return null; // Return null if no match is found
+	}
 };
 
 export default memo(PostPreview);
