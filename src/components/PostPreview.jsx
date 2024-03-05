f4e2a1d8eb0a1a73404e7059b77d61d585b4c5ab
Jay <japusen32@gmail.com>
2024-03-04 22:32:29 -0800

    Refactored to combine similar post types

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -32,10 +32,17 @@ const styles = StyleSheet.create({
 	separator: {
 		marginTop: 10,
 	},
-	thumbnail: {
+	thumbnailContainer: {
+		overflow: "hidden",
+		borderRadius: 10,
 		height: 100,
 		width: 100,
-		borderRadius: 10,
+	},
+	thumbnail: { flex: 1 },
+	thumbnailPlaceholder: {
+		flex: 1,
+		width: 100,
+		height: 100,
 	},
 	fullImage: {
 		height: 500,
@@ -63,6 +70,13 @@ export const PostPreview = ({ item }) => {
 			return (
 				<PreviewCard>
 					<Header post={item} toggleShowMedia={toggleShowMedia} />
+					{showMedia && (
+						<View style={{ display: "flex", gap: 10 }}>
+							{item.gallery.map((data) => (
+								<RedditImage key={data.id} url={data.url} />
+							))}
+						</View>
+					)}
 				</PreviewCard>
 			);
 		case "image":
@@ -83,7 +97,7 @@ export const PostPreview = ({ item }) => {
 			return (
 				<PreviewCard>
 					<Header post={item} toggleShowMedia={toggleShowMedia} />
-					{showMedia && <ExternalVideo url={item.videoURL} />}
+					{showMedia && <WebviewVideo url={item.videoURL} />}
 				</PreviewCard>
 			);
 		case "link":
@@ -94,53 +108,62 @@ export const PostPreview = ({ item }) => {
 				</PreviewCard>
 			);
 		default:
-			return <Header post={item} toggleShowMedia={() => {}} />;
+			return (
+				<PreviewCard>
+					<Header post={item} toggleShowMedia={() => {}} />
+				</PreviewCard>
+			);
 	}
 };
 
 const PreviewCard = ({ id, children }) => {
-	const navigation = useNavigation();
 	const theme = useTheme();
 
 	return (
-		<Card
-			style={[styles.card, { backgroundColor: theme.colors.surface }]}
-			onPress={() => {
-				// navigation.navigate("Post", {
-				// 	postID: id,
-				// });
-			}}
-		>
+		<Card style={[styles.card, { backgroundColor: theme.colors.surface }]}>
 			{children}
 		</Card>
 	);
 };
 
 const Header = ({ post, toggleShowMedia }) => {
+	const navigation = useNavigation();
 	const theme = useTheme();
 
 	return (
-		<View style={styles.postHeader}>
-			{post.thumbnail && (
-				<Thumbnail url={post.thumbnail} onPress={toggleShowMedia} />
-			)}
-			<View style={styles.postInfo}>
-				<Text style={{ color: theme.colors.primary }}>
-					{post.title}
-				</Text>
-				<Text>{post.subreddit}</Text>
-				<Text>{post.author}</Text>
-				<Text>{post.score}</Text>
-				<Indicators
-					isNsfw={post.isNsfw}
-					isSpoiler={post.isSpoiler}
-					isLocked={post.isLocked}
-					isPinned={post.isPinned}
-					isStickied={post.isStickied}
-				/>
-				<Text>Type: {post.type}</Text>
+		<Pressable
+			onPress={() => {
+				// navigation.navigate("Post", {
+				// 	postID: id,
+				// });
+			}}
+		>
+			<View style={styles.postHeader}>
+				{/* {post.thumbnail && (
+					<Thumbnail url={post.thumbnail} onPress={toggleShowMedia} />
+				)} */}
+				{post.type !== "self" && (
+					<Thumbnail url={post.thumbnail} onPress={toggleShowMedia} />
+				)}
+				<View style={styles.postInfo}>
+					<Text style={{ color: theme.colors.primary }}>
+						{post.title}
+					</Text>
+					<Text>{post.subreddit}</Text>
+					<Text>{post.author}</Text>
+					<Text>{post.score}</Text>
+					<Indicators
+						isNsfw={post.isNsfw}
+						isSpoiler={post.isSpoiler}
+						isLocked={post.isLocked}
+						isPinned={post.isPinned}
+						isStickied={post.isStickied}
+					/>
+					<Text>Type: {post.type}</Text>
+					<Text>url: {post.videoURL && post.videoURL}</Text>
+				</View>
 			</View>
-		</View>
+		</Pressable>
 	);
 };
 
@@ -158,14 +181,22 @@ const Indicators = ({ isNsfw, isSpoiler, isLocked, isPinned, isStickied }) => {
 
 const Thumbnail = ({ url, onPress }) => {
 	return (
-		<Pressable onPress={onPress}>
-			<Image
-				resizeMode="cover"
-				style={styles.thumbnail}
-				source={{
-					uri: url,
-				}}
-			/>
+		<Pressable onPress={onPress} style={styles.thumbnailContainer}>
+			{url ? (
+				<Image
+					resizeMode="cover"
+					style={styles.thumbnail}
+					source={{
+						uri: url,
+					}}
+				/>
+			) : (
+				<Image
+					resizeMode="contain"
+					style={styles.thumbnailPlaceholder}
+					source={require("../../assets/play-circle-outline.png")}
+				/>
+			)}
 		</Pressable>
 	);
 };
@@ -204,38 +235,14 @@ const RedditVideo = ({ url }) => {
 	);
 };
 
-const ExternalVideo = ({ url }) => {
+const WebviewVideo = ({ url }) => {
 	return (
 		<WebView
-			// originWhitelist={["*"]}
 			allowsFullscreenVideo
 			allowsInlineMediaPlayback
 			mediaPlaybackRequiresUserAction
-			// source={{ html: media.oembed.html }}
 			source={{ uri: url }}
 			style={styles.externalVideo}
-		/>
-	);
-};
-
-const ImgurVideo = () => {
-	const [isMute, setIsMute] = useState(false);
-	return (
-		<VideoPlayer
-			videoProps={{
-				shouldPlay: true,
-				resizeMode: ResizeMode.CONTAIN,
-				source: {
-					uri: item.url.replace(".gifv", ".mp4"),
-				},
-				isMuted: isMute,
-			}}
-			mute={{
-				enterMute: () => setIsMute(!isMute),
-				exitMute: () => setIsMute(!isMute),
-				isMute,
-			}}
-			style={{ height: 500 }}
 		/>
 	);
 };
