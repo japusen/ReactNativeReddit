9a12482033ab5a54dcea1061e3241bc7d1ec7594
Jay <japusen32@gmail.com>
2024-03-04 22:34:15 -0800

    Fixed thumbnails edge cases. Added link handling for various domains such as youtube, imgur, etc

--- src/utils/ParsePost.js
+++ src/utils/ParsePost.js
@@ -25,11 +25,14 @@ const parsePost = (post) => {
 	}
 
 	if (post.is_gallery) {
-		// thumbnail can be "nsfw"
+		const gallery = parseGallery(post.media_metadata, post.gallery_data);
 		return {
 			...base,
 			type: "gallery",
-			thumbnail: post.thumbnail === "nsfw" ? null : post.thumbnail,
+			thumbnail: isValidThumbnail(post.thumbnail)
+				? post.thumbnail
+				: gallery[0].url,
+			gallery,
 		};
 	}
 
@@ -38,7 +41,11 @@ const parsePost = (post) => {
 			return {
 				...base,
 				type: "image",
-				thumbnail: parseThumbnail(post.thumbnail, post.preview),
+				thumbnail: parseThumbnail(
+					post.thumbnail,
+					post.preview,
+					post.url
+				),
 				imageURL: post.url,
 			};
 		case "hosted:video":
@@ -56,6 +63,14 @@ const parsePost = (post) => {
 				videoURL: extractVideoSrcFromHTML(post.media.oembed.html),
 			};
 		case "link":
+			if (post.domain === "i.imgur.com" && post.url.includes(".gifv")) {
+				return {
+					...base,
+					type: "reddit_video",
+					thumbnail: parseThumbnail(post.thumbnail, post.preview),
+					videoURL: post.url.replace(".gifv", ".mp4"),
+				};
+			}
 			return {
 				...base,
 				type: "link",
@@ -63,20 +78,81 @@ const parsePost = (post) => {
 				link: post.url,
 			};
 		default:
-			// post with a link (can be seen in r/science)
-			return {
-				...base,
-				type: "legacy",
-				link: post.url,
-			};
+			// post_hint not used in some subreddits ex: anime_irl
+			if (post.is_reddit_media_domain && post.is_video) {
+				return {
+					...base,
+					type: "reddit_video",
+					thumbnail: parseThumbnail(post.thumbnail, post.preview),
+					videoURL: post.media.reddit_video.dash_url,
+				};
+			} else if (post.is_reddit_media_domain && !post.is_video) {
+				return {
+					...base,
+					type: "image",
+					thumbnail: parseThumbnail(
+						post.thumbnail,
+						post.preview,
+						post.url
+					),
+					imageURL: post.url,
+				};
+			} else if (post.domain === "youtube.com") {
+				const id = post.url.split("?v=")[1];
+				return {
+					...base,
+					type: "external_video",
+					thumbnail: null,
+					videoURL: `https://www.youtube.com/embed/${id}`,
+				};
+			} else if (post.domain === "pornhub.com") {
+				const id = post.url.split("?viewkey=")[1];
+				return {
+					...base,
+					type: "external_video",
+					thumbnail: parseThumbnail(post.thumbnail, post.preview),
+					videoURL: `https://www.pornhub.com/embed/${id}`,
+				};
+			} else {
+				return {
+					...base,
+					type: "unhandled",
+					link: post.url,
+					thumbnail: parseThumbnail(
+						post.thumbnail,
+						post.preview,
+						post.url
+					),
+				};
+			}
 	}
 };
 
-const parseThumbnail = (thumbnail, preview) => {
+const parseGallery = (metaData, galleryData) => {
+	return galleryData.items.map((item) => {
+		const id = item.media_id;
+		const ext = metaData[id].m.split("/")[1];
+		return { id, url: "https://i.redd.it/".concat(id, `.${ext}`) };
+	});
+};
+
+const parseThumbnail = (thumbnail, preview, url = null) => {
 	// thumbnail can be default
-	return thumbnail && thumbnail !== "default"
+	return isValidThumbnail(thumbnail)
 		? thumbnail
-		: preview.images.at(0).source.url;
+		: preview
+		? preview.images.at(0).source.url
+		: url;
+};
+
+const isValidThumbnail = (thumbnail) => {
+	return (
+		thumbnail &&
+		thumbnail !== "default" &&
+		thumbnail !== "nsfw" &&
+		thumbnail !== "image" &&
+		thumbnail !== "spoiler"
+	);
 };
 
 const extractVideoSrcFromHTML = (htmlString) => {
