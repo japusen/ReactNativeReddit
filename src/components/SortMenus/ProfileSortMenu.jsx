63aeba716346ca41fc79e93df6e141ffdd8017da
Jay <japusen32@gmail.com>
2024-04-11 01:07:50 -0700

    Changed icon of menu to 'menu swap'

--- src/components/SortMenus/ProfileSortMenu.jsx
+++ src/components/SortMenus/ProfileSortMenu.jsx
@@ -1,12 +1,12 @@
 import SortMenu from "./SortMenu";
 
-const ProfileSortMenu = ({ setSort }) => {
+const ProfileSortMenu = ({ setType }) => {
 	const sorts = [
 		{ name: "submitted", title: "Submissions", icon: "post-outline" },
-		{ name: "comments", title: "Comments", icon: "comment" },
+		{ name: "comments", title: "Comments", icon: "comment-outline" },
 	];
 
-	return <SortMenu sorts={sorts} setSort={setSort} />;
+	return <SortMenu sorts={sorts} setSort={setType} icon="menu-swap" />;
 };
 
 export default ProfileSortMenu;
