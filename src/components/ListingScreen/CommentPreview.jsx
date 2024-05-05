951c0abd973ad3e0e157c4fe5db7d7b892a8962c
Jay <japusen32@gmail.com>
2024-05-04 22:47:15 -0700

    Modified styling. Added navigation to comment thread

--- src/components/ListingScreen/CommentPreview.jsx
+++ src/components/ListingScreen/CommentPreview.jsx
@@ -1,5 +1,6 @@
-import { View, StyleSheet } from "react-native";
+import { Pressable, View, StyleSheet } from "react-native";
 import { Card, Divider, Text, useTheme } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
 	card: {
@@ -11,31 +12,52 @@ const styles = StyleSheet.create({
 
 const CommentPreview = ({ comment }) => {
 	const theme = useTheme();
+	const navigation = useNavigation();
+
 	return (
-		<Card style={[styles.card, { backgroundColor: theme.colors.surface }]}>
-			<View style={{ display: "flex", gap: 4 }}>
-				<Text>{comment.title}</Text>
-				<View
-					style={{
-						display: "flex",
-						flexDirection: "row",
-						gap: 8,
-					}}
-				>
-					<Text>{comment.subreddit}</Text>
-					<Text>{comment.score}</Text>
-					<Text>{comment.time}</Text>
+		<Pressable
+			onPress={() => {
+				navigation.navigate("Post", {
+					postID: comment.linkID,
+					subreddit: comment.subreddit,
+					specificComment: comment.id,
+				});
+			}}
+		>
+			<Card
+				style={[styles.card, { backgroundColor: theme.colors.surface }]}
+			>
+				<View style={{ display: "flex", gap: 4 }}>
+					<Text
+						variant="titleSmall"
+						style={{ color: theme.colors.primary }}
+					>
+						{comment.title}
+					</Text>
+					<View
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							gap: 8,
+						}}
+					>
+						<Text>{comment.subreddit}</Text>
+						<Text>{comment.score}</Text>
+						<Text>{comment.time}</Text>
+					</View>
+					<Divider
+						bold
+						style={{
+							marginVertical: 4,
+							backgroundColor: theme.colors.secondary,
+						}}
+					/>
+					<Text selectable variant="bodyLarge">
+						{comment.body}
+					</Text>
 				</View>
-				<Divider
-					bold
-					style={{
-						marginVertical: 4,
-						backgroundColor: theme.colors.primary,
-					}}
-				/>
-				<Text>{comment.body}</Text>
-			</View>
-		</Card>
+			</Card>
+		</Pressable>
 	);
 };
 
